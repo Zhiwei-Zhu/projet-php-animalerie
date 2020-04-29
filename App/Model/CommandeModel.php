92120ce29a74a48bd3f962d6659924808002cf59
Vincent Zhu <zhu.zhiwei.vincent@gmail.com>
2020-04-29 19:36:10 +0200

    add model

---
+++ App/Model/CommandeModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Model;
+
+use Core\Model\Query;
+
+class CommandeModel extends Query{
+    
+}
---
+++ App/Model/ProduitModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Model;
+
+use Core\Model\Query;
+
+class ProduitModel extends Query{
+    
+}
