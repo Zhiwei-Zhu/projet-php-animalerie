9f3a78d9781662ff72890c26ed7b7ff594245144
KevZh-hub <55091168+KevZh-hub@users.noreply.github.com>
2020-05-03 19:22:44 +0200

    error endif corrigé

--- App/View/Produit/indexView.php
+++ App/View/Produit/indexView.php
@@ -4,11 +4,10 @@
     <div class="card" style="width: 18rem;">
     <?php if ($produit->type_animal == "chien") ?>
         <img class="card-img-top" src="https://fakeimg.pl/300x200/" alt="Produit pour chien">
-    <?php elseif($produit->type_animal == "chat") ?>
+    <?php if($produit->type_animal == "chat") ?>
         <img class="card-img-top" src="https://fakeimg.pl/300x200/" alt="Produit pour chat">
-    <?php elseif($produit->type_animal == "oiseaux") ?>
+    <?php if($produit->type_animal == "oiseaux") ?>
     <img class="card-img-top" src="https://fakeimg.pl/300x200/" alt="Produit pour oiseaux">
-    <?php endif ?>
         <div class="card-body"> 
             <h5 class="card-title"></h5>
             <div class="card-text">
