fe0ab3cdc1c21149ab87e68431c8db1823046e5e
Guillonaxel0714 <55197055+Guillonaxel0714@users.noreply.github.com>
2020-05-04 00:10:36 +0200

    update carousel

--- App/View/Home/indexView.php
+++ App/View/Home/indexView.php
@@ -11,7 +11,7 @@
 
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-        <?php foreach ($animals as $animal) : ?>
+    <?php foreach ($animals as $animal) : ?>
         <?php if($animal->type === "chat"): ?>
         <div class="carousel-item active">
             <img src="http://www.leparisien.fr/resizer/74wMQIeXKLDUKuKvSzJQME_0ULg=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/KNVYKZBDTTOXNO37FEVDTD5DJU.jpg" class="d-block w-100" alt="chat">
@@ -27,6 +27,7 @@
                 <img src="https://secure.img1-fg.wfcdn.com/im/07292327/resize-h800-w800%5Ecompr-r85/1166/116615321/Jeremy+Artful+12+in+x+6+in+x+8+in+Birdhouse.jpg" class="d-block w-100" alt="chat">
             </div>
             <?php endif ?>
+            <?php endforeach ?>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -37,37 +38,40 @@
         <span class="sr-only">Next</span>
     </a>
 
-    <?php endforeach ?>
 </div>
 
 
-
-<div class="recherche mt-5"></div>
-<div class="affichage mt-5 justify-content-around">
-<?php foreach ($produits as $produit) :?>
-    <div class="card" style="width: 18rem;">
-    <?php if ($produit->type_animal === "chien"): ?>
-        <img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/07/22/13/21/kibble-855419_960_720.jpg" alt="Produit pour chien">
-        <?php endif; ?>
-    <?php if ($produit->type_animal === "chat"): ?>
-        <img class="card-img-top" src="https://image.shutterstock.com/image-photo/ceramic-bowl-dried-pet-food-260nw-1047394765.jpg" alt="Produit pour chat">
-        <?php endif; ?>
-    <?php if ($produit->type_animal === "oiseaux"): ?>
-        <img class="card-img-top" src="https://secure.img1-fg.wfcdn.com/im/07292327/resize-h800-w800%5Ecompr-r85/1166/116615321/Jeremy+Artful+12+in+x+6+in+x+8+in+Birdhouse.jpg" alt="Produit pour oiseaux">
-        <?php endif; ?>
-        <div class="card-body">
-            <h5 class="card-title"></h5>
-            <div class="card-text">
-                <p>Nom: <?= $produit->nom ?></p>
-                <p>Catégorie: <?= $produit->type_animal ?></p>
-                <p>Prix: <?= $produit->prix ?></p>
-                <p>Stock: <?= $produit->stock ?> </p>
-            </div>
-            <a href="<?= $produit->id ?>" class="btn btn-primary">Voir en détail</a>
+<div class="container">
+        <div class="row">
+            <?php foreach ($produits as $produit) :?>
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                    <?php if ($produit->type_animal === "chien"): ?>
+                        <img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/07/22/13/21/kibble-855419_960_720.jpg" alt="Produit pour chien">
+                        <?php endif; ?>
+                    <?php if ($produit->type_animal === "chat"): ?>
+                        <img class="card-img-top" src="https://image.shutterstock.com/image-photo/ceramic-bowl-dried-pet-food-260nw-1047394765.jpg" alt="Produit pour chat">
+                        <?php endif; ?>
+                    <?php if ($produit->type_animal === "oiseaux"): ?>
+                        <img class="card-img-top" src="https://secure.img1-fg.wfcdn.com/im/07292327/resize-h800-w800%5Ecompr-r85/1166/116615321/Jeremy+Artful+12+in+x+6+in+x+8+in+Birdhouse.jpg" alt="Produit pour oiseaux">
+                        <?php endif; ?>
+                        <div class="card-body"> 
+                            <h5 class="card-title"></h5>
+                            <div class="card-text">
+                                <p>Nom: <?= $produit->nom ?></p>
+                                <p>Catégorie: <?= $produit->type_animal ?></p>
+                                <p>Prix: <?= $produit->prix ?></p>
+                                <p>Stock: <?= $produit->stock ?> </p>
+                            </div>
+                            <a href="index.php?page=singleProduit&id=<?= $produit->id ?>" class="btn btn-primary">Voir en détail</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach ?>
         </div>
     </div>
 </div>
-<?php endforeach ?>
+         
 
 
 <div class="container mt-5">
