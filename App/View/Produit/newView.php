c2493bf18c8b917bc477f0556339a9e9332f2547
KevZh-hub <55091168+KevZh-hub@users.noreply.github.com>
2020-05-03 16:51:06 +0200

    Update newView.php
    update newview produit

--- App/View/Produit/newView.php
+++ App/View/Produit/newView.php
@@ -1,20 +1,20 @@
 <div class="container mt-5">
-<form action="index.php?page=newProduit=<?= $produit->id ?>" method="POST">
+<form action="index.php?page=newProduit=" method="POST">
 <div class="form-group">
         <label for="Nom">Nom</label>
-        <input type="text" class="form-control" id="Nom" name="Nom" value="<?= $produit->nom ?>">
+        <input type="text" class="form-control" id="Nom" name="Nom" value="">
     </div>
     <div class="form-group">
         <label for="Type_animal">Catégorie</label>
-        <input type="text" class="form-control" id="Type_animal" name="Type_animal" value="<?= $produit->type_animal ?>">
+        <input type="text" class="form-control" id="Type_animal" name="Type_animal" value="">
     </div>
     <div class="form-group">
         <label for="Prix">Prix</label>
-        <input type="number" class="form-control" id="Prix" name="Prix" value="<?= $produit->prix ?>">
+        <input type="number" class="form-control" id="Prix" name="Prix" value="">
     </div>
     <div class="form-group">
         <label for="Stock">Stock</label>
-        <input type="number" class="form-control" id="Stock" name="Stock" value="<?= $produit->stock ?>">
+        <input type="number" class="form-control" id="Stock" name="Stock" value="">
     </div>
         <button type="submit" class="btn btn-primary">Enregistrer</button>
 </form>
