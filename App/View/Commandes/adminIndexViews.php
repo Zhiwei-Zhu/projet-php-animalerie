1537f9c782033292b6c1d29a9aa60865b16c4c1f
Guillonaxel0714 <55197055+Guillonaxel0714@users.noreply.github.com>
2020-05-03 18:06:18 +0200

    adminIndexViews update

--- App/View/Commandes/adminIndexViews.php
+++ App/View/Commandes/adminIndexViews.php
@@ -1,3 +1,19 @@
-<?php
 
-?>
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">User</th>
+            <th scope="col">Nom animal</th>
+            <th scope="col">Date RDV</th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($reservations as $reservation) : ?>
+                <tr>
+                    <td><?php $reservation->user_id->nom ?></td>
+                    <td><?php $reservation->animal_id->nom ?></td>
+                    <td><?php $reservation->daterdv ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
