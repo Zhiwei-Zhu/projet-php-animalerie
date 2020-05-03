7846ae371860e0fdd219fe1256f9fda1ab47d32c
Tirdys <55100702+Tirdys@users.noreply.github.com>
2020-05-03 15:57:20 +0200

    add modif

--- App/View/user/login.php
+++ App/View/user/login.php
@@ -1,11 +1,11 @@
 <div class="container mt-5">
     <form action="index.php?page=login" method="POST">
         <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" name="username">
+            <label for="nom">Nom :</label>
+            <input type="text" class="form-control" id="nom" name="nom">
         </div>
         <div class="form-group">
-            <label for="password">Adresse</label>
+            <label for="password">Mot de passe :</label>
             <input type="password" class="form-control" id="password" name="password">
         </div>
         <button type="submit" class="btn btn-primary">Enregistrer</button>
--- App/View/user/signup.php
+++ App/View/user/signup.php
@@ -2,11 +2,42 @@
     <form action="index.php?page=signup" method="POST">
         <p class=text-danger><?= $message ?></p>
         <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" name="username">
+            <label for="nom">Nom</label>
+            <input type="text" class="form-control" id="nom" name="nom">
         </div>
         <div class="form-group">
-            <label for="password">Adresse</label>
+            <label for="prenom">Prenom</label>
+            <input type="text" class="form-control" id="prenom" name="prenom">
+        </div>
+        <div class="form-group">
+            <label for="adresse">Adresse</label>
+            <input type="text" class="form-control" id="adresse" name="adresse">
+        </div>
+        <div class="form-group">
+            <label for="ville">Ville</label>
+            <input type="text" class="form-control" id="ville" name="ville">
+        </div>
+        <div class="form-group">
+            <label for="code_postal">Code Postal</label>
+            <input type="text" class="form-control" id="code_postal" name="code_postal">
+        </div>
+        <div class="form-group">
+            <label for="mail">Mail</label>
+            <input type="email" class="form-control" id="mail" name="mail">
+        </div>
+        <div class="form-group">
+            <label for="telephone">Telephone</label>
+            <input type="tel" class="form-control" id="telephone" name="telephone">
+        </div>
+        <div class="form-group">
+            <label for="role">Rôle</label>
+            <select type="text" class="form-control" id="role" name="role">
+            <option value="non-admin">No admin</option>
+            <option value="admin">admin</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
             <input type="password" class="form-control" id="password" name="password">
         </div>
         <button type="submit" class="btn btn-primary">Enregistrer</button>
