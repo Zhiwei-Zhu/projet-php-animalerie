a674e641dcbaf80734b4ae8d7a06767e903cc754
KevZh-hub <55091168+KevZh-hub@users.noreply.github.com>
2020-05-03 17:25:25 +0200

    update

--- App/View/Produit/singleView.php
+++ App/View/Produit/singleView.php
@@ -1,7 +1,13 @@
 <div class="recherche mt-5"></div>
 <div class="affichage mt-5 justify-content-around">
     <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="https://fakeimg.pl/300x200/" alt="Card image cap">
+    <?php if ($produit->type_animal == "chien") ?>
+        <img class="card-img-top" src="https://fakeimg.pl/300x200/" alt="Produit pour chien">
+    <?php elseif($produit->type_animal == "chat") ?>
+        <img class="card-img-top" src="https://fakeimg.pl/300x200/" alt="Produit pour chat">
+    <?php elseif($produit->type_animal == "oiseaux") ?>
+    <img class="card-img-top" src="https://fakeimg.pl/300x200/" alt="Produit pour oiseaux">
+    <?php endif ?>
         <div class="card-body">
             <h5 class="card-title"></h5>
             <div class="card-text">
@@ -15,7 +21,7 @@
             <?php elseif ($_SESSION["user"]->role=="admin"): ?> 
                 <a href="index.php?page=modifyAnimal&id=<?= $produit->id ?>" class="btn btn-primary">Modifier</a>
                 <a href="index.php?page=deleteAnimal&id=<?= $produit->id ?>" class="btn btn-danger">Supprimer</a>
-            <?php elseif (isset($_SESSION["user"])): ?>
+            <?php elseif (!isset($_SESSION["user"])): ?>
                 <p>Veuillez vous connecter pour ajouter un produit au panier</p>
                 <a href="" alt="" class="btn btn-secondary">Ajouter au panier</a>
             <?php endif; ?>
