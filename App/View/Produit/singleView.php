09198cd599def03aedf8b672947ee8d5845df14e
KevZh-hub <55091168+KevZh-hub@users.noreply.github.com>
2020-05-03 19:33:24 +0200

    update

--- App/View/Produit/singleView.php
+++ App/View/Produit/singleView.php
@@ -1,13 +1,15 @@
 <div class="recherche mt-5"></div>
 <div class="affichage mt-5 justify-content-around">
     <div class="card" style="width: 18rem;">
-    <?php if ($produit->type_animal == "chien") ?>
+    <?php if ($produit->type_animal === "chien"): ?>
         <img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/07/22/13/21/kibble-855419_960_720.jpg" alt="Produit pour chien">
-    <?php if ($produit->type_animal == "chat") ?>
+        <?php endif; ?>
+    <?php if ($produit->type_animal === "chat"): ?>
         <img class="card-img-top" src="https://image.shutterstock.com/image-photo/ceramic-bowl-dried-pet-food-260nw-1047394765.jpg" alt="Produit pour chat">
-    <?php if ($produit->type_animal == "oiseaux") ?>
-    <img class="card-img-top" src="https://secure.img1-fg.wfcdn.com/im/07292327/resize-h800-w800%5Ecompr-r85/1166/116615321/Jeremy+Artful+12+in+x+6+in+x+8+in+Birdhouse.jpg" alt="Produit pour oiseaux">
-        <div class="card-body">
+        <?php endif; ?>
+    <?php if ($produit->type_animal === "oiseaux"): ?>
+        <img class="card-img-top" src="https://secure.img1-fg.wfcdn.com/im/07292327/resize-h800-w800%5Ecompr-r85/1166/116615321/Jeremy+Artful+12+in+x+6+in+x+8+in+Birdhouse.jpg" alt="Produit pour oiseaux">
+        <?php endif; ?>
             <h5 class="card-title"></h5>
             <div class="card-text">
                 <p>Nom: <?= $produit->nom ?></p>
